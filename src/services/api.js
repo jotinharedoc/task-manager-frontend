cf9502032afb422edb57178d5a0c913e073f2f34
Jotinha <jotajotaj710@gmail.com>
2026-04-22 23:05:44 -0300

    fix: url da api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: import.meta.env.VITE_API_URL || 'https://task-manager-api-production-e473.up.railway.app'
 });
 
 api.interceptors.request.use((config) => {
